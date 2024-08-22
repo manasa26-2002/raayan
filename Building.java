add7835fde1e447ab2646130f38672abc6b3de21
Manu Gouda <manasans.xworkz@gmail.com>
2024-08-22 19:52:11 +0530

    program saved

---
+++ Building.java
@@ -0,0 +1,17 @@
+class Building{
+public static void main(String [] args){
+int noOfFloors=3;
+System.out.println(" no Of Floors="+noOfFloors);
+int noOfFloors2=30;
+System.out.println(" no Of Floors2="+noOfFloors2);
+char nameOfTheBuilding='h';
+System.out.println("name of the building="+nameOfTheBuilding);
+char nameOfTheBuilding3='z';
+System.out.println("name of the building4="+nameOfTheBuilding3);
+double Peoples=20000d;
+System.out.println("peoples="+Peoples);
+double Peoples4=40000d;
+System.out.println("peoples5="+Peoples4);
+}
+
+}
